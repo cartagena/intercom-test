86f58619c7fbc6ff6d98d34ece32663c8bc46f37
Miguel Cartagena <miguelcartagena@gmail.com>
2016-07-02 10:47:31 +0100

    Function for flat nested arrays complete

--- src/main/java/com/github/cartagena/intercom/ArrayOperations.java
+++ src/main/java/com/github/cartagena/intercom/ArrayOperations.java
@@ -1,10 +1,49 @@
 package com.github.cartagena.intercom;
 
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
+import static java.util.Objects.isNull;
+
 public class ArrayOperations {
 
+    /**
+     * Flats an array of arbitrary nested arrays of {@link Integer}.
+     *
+     * @param array an array of {@link Object} containing only integers or nested array(s) of integers.
+     * @return a flattened array of {@link Integer}
+     * @throws IllegalAccessException if the input array if invalid, ie, is null or contains elements other than
+     * integers and nested array(s) of integers.
+     */
+    public Integer[] flatIntegerArray(Object[] array) {
+        if(isNull(array)) {
+            throw new IllegalArgumentException("Argument should not be null.");
+        }
 
-    public int[] flatArray(Object[] array) {
-        return null;
+        ArrayList<Integer> flattenedList = new ArrayList<>();
+
+        for(Object element : array) {
+            if(!isValidElement(element)) {
+                throw new IllegalArgumentException("Argument should contain only integers or array of integers.");
+            }
+
+            if(isNestedArray(element)) {
+                Integer[] n = flatIntegerArray((Object[]) element);
+                flattenedList.addAll(asList(n));
+            } else {
+                flattenedList.add((Integer) element);
+            }
+        }
+
+        return flattenedList.toArray(new Integer[flattenedList.size()]);
+    }
+
+    private boolean isValidElement(Object element) {
+        return element instanceof Integer || isNestedArray(element);
+    }
+
+    private boolean isNestedArray(Object element) {
+        return element instanceof Object[];
     }
 
 }
--- src/test/java/com/github/cartagena/intercom/ArrayOperationsTest.java
+++ src/test/java/com/github/cartagena/intercom/ArrayOperationsTest.java
@@ -3,22 +3,170 @@ package com.github.cartagena.intercom;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 public class ArrayOperationsTest {
 
+    private ArrayOperations operations = new ArrayOperations();
+
     @Test
     public void shouldFlattenSimpleNestedArray() {
-        Object[] nestedArray = new Object[2];
-        nestedArray[0] = 1;
-        nestedArray[1] = new Object[] { 2, 3 };
+        Object[] nestedArray = new Object[] {
+            1,
+            new Object[] { 2, 3 }
+        };
 
-        ArrayOperations operations = new ArrayOperations();
-        int[] flattedArray = operations.flatArray(nestedArray);
+        Integer[] flattenedArray = operations.flatIntegerArray(nestedArray);
 
-        assertThat(flattedArray)
+        assertThat(flattenedArray)
                 .hasSize(3)
                 .containsExactly(1, 2, 3);
 
     }
 
+    @Test
+    public void shouldFlattenDoubleNestedArray() {
+        Object[] nestedArray = new Object[] {
+                1,
+                new Object[]{ 2, 3 },
+                new Object[]{
+                        new Object[]{ 4 },
+                        5, 6}
+        };
+
+        Integer[] flattenedArray = operations.flatIntegerArray(nestedArray);
+
+        assertThat(flattenedArray)
+                .hasSize(6)
+                .containsExactly(1, 2, 3, 4, 5, 6);
+
+    }
+
+    @Test
+    public void shouldFlattenSimpleDeepNestedArray() {
+        Object[] nestedArray = new Object[] {
+                1,
+                new Object[]{
+                        new Object[]{
+                                new Object[]{
+                                        new Object[]{
+                                                new Object[]{
+                                                        new Object[]{
+                                                                new Object[]{
+                                                                        new Object[]{
+                                                                                new Object[]{
+                                                                                        new Object[]{
+                                                                                                new Object[]{
+                                                                                                        new Object[]{2}
+                                                                                                }
+                                                                                        }
+                                                                                }
+                                                                        }
+                                                                }
+                                                        }
+                                                }
+                                        }
+                                }
+                        },
+                }
+        };
+
+        Integer[] flattenedArray = operations.flatIntegerArray(nestedArray);
+
+        assertThat(flattenedArray)
+                .hasSize(2)
+                .containsExactly(1, 2);
+
+    }
+
+    @Test
+    public void shouldThrowExceptionIfNestedArrayDoesNotContainOnlyIntegers() {
+        Object[] nestedArray = new Object[] {
+                1,
+                new Object[] { 2, "String" }
+        };
+
+        try {
+            operations.flatIntegerArray(nestedArray);
+            fail("Expecting exception when argument is not composed only by integers.");
+        } catch(Exception e) {
+            assertThat(e)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("Argument should contain only integers or array of integers.");
+        }
+
+    }
+
+    @Test
+    public void shouldThrowExceptionIfInputArrayDoesNotContainOnlyIntegers() {
+        Object[] nestedArray = new Object[] {
+                1,
+                "String"
+        };
+
+        try {
+            operations.flatIntegerArray(nestedArray);
+            fail("Expecting exception when argument is not composed only by integers or nested array of integers.");
+        } catch(Exception e) {
+            assertThat(e)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("Argument should contain only integers or array of integers.");
+        }
+
+    }
+
+    @Test
+    public void shouldReturnEmptyArrayIfInputIsEmpty() {
+        Integer[] flattenedArray = operations.flatIntegerArray(new Object[0]);
+
+        assertThat(flattenedArray)
+                .isEmpty();
+
+    }
+
+    @Test
+    public void shouldThrowExceptionIfInputArrayIsNull() {
+        try {
+            operations.flatIntegerArray(null);
+            fail("Expecting exception when argument is null.");
+        } catch(Exception e) {
+            assertThat(e)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("Argument should not be null.");
+        }
+
+    }
+
+    @Test
+    public void shouldThrowExceptionIfNestedElementIsNull() {
+        Object[] nestedArray = new Object[] {
+                1,
+                new Object[] { null }
+        };
+
+        try {
+            operations.flatIntegerArray(nestedArray);
+            fail("Expecting exception when argument is not composed only by integers or nested array of integers.");
+        } catch(Exception e) {
+            assertThat(e)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("Argument should contain only integers or array of integers.");
+        }
+    }
+
+    @Test
+    public void shouldFlattenSimpleNestedArrayWithEmptyNestedArray() {
+        Object[] nestedArray = new Object[] {
+                1,
+                new Object[] { 2, 3 },
+                new Object[0]
+        };
+
+        Integer[] flattenedArray = operations.flatIntegerArray(nestedArray);
+
+        assertThat(flattenedArray)
+                .hasSize(3)
+                .containsExactly(1, 2, 3);
+
+    }
 }
